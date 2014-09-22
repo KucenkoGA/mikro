49c8cdbd5aa366eb5e5a3b854e2c8808193349f7
KucenkoGA <Vtufkty@yandex.ru>
2014-09-22 19:50:00 +0600

    Lab1 Triangle

---
+++ Brylova_Kucenko/Triangle/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+//C:\Documents and Settings\User\Рабочий стол\Triangle\tri.txt
+namespace Triangle
+{
+    public class Program
+    {
+        //считывает строку, если все ок - записывает в файл результат
+        //выводит инфо в лог
+        static void ReadNextLine(StreamReader sr, StreamWriter sw)
+        {
+            if(!sr.EndOfStream)
+            { 
+                double a = 0, b = 0, c = 0, alf = 0, bet = 0, gam = 0;
+                Triangle t = new Triangle();
+                string fine_read = "ok";
+                string line = sr.ReadLine();
+                try
+                {
+                    char[] ch = new char[1] { ';' };
+                    string[] value = line.Split(ch);
+
+                    a = Convert.ToDouble(value[0]);
+                    b = Convert.ToDouble(value[1]);
+                    alf = Convert.ToDouble(value[2]);
+                }
+                catch 
+                {
+                    fine_read = "incorrect string " + line;
+                }
+                if (fine_read == "ok")
+                {
+                    string res = FindValue(a, b, alf, t);
+                    if (res == "ok")
+                    {
+                        c = t.GetC();
+                        bet = t.GetBeta();
+                        gam = t.GetGamma();
+                        sw.WriteLine(a.ToString("0.000") + ';' + b.ToString("0.000") + ';' + c.ToString("0.000"));
+                        Console.WriteLine("betta = {0:0.000}grad.; gamma = {1:0.000}grad.", bet, gam);
+                    }
+                    else
+                        Console.WriteLine(res);
+                }
+                else
+                    Console.WriteLine(fine_read);
+            }
+        }
+        static string FindValue(double A, double B, double Alfa, Triangle t)
+        {
+            string s1 = t.SetA(A);
+            if (s1 == "ok")
+            {
+                string s2 = t.SetB(B);
+                if (s2 == "ok")
+                {
+                    string s3 = t.SetAlfa(Alfa);
+                    if (s3 == "ok")
+                    {
+                        t.FindC();
+                        t.FindBeta(); 
+                        t.FindLast();
+                        return "ok";
+                    }
+                    else
+                        return s3;
+                }
+                else
+                    return s2;
+            }
+            else
+                return s1;
+        }
+
+        public static void Main(string[] args)
+        {
+            string file_in = "",
+                   file_out = "";
+            //file_in = Console.ReadLine();
+            //file_out = Console.ReadLine();
+            file_in = args[0];
+            file_out = args[1];
+            try
+            {
+                StreamReader sr = new StreamReader(file_in);
+                StreamWriter sw = new StreamWriter(file_out);
+                while (!sr.EndOfStream)
+                {
+                    ReadNextLine(sr, sw);
+                }
+                sr.Close();
+                sw.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("file {0} not found", file_in);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Brylova_Kucenko/Unit_Test/Unit_Test.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace Unit_Test
+{
+    [TestClass]
+    public class Unit_Test
+    {
+        [TestMethod]
+        public void TestMethod_C1()                 // для FindC()
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(4);
+            tr.SetAlfa(90);
+
+            tr.FindC();
+            double c = tr.GetC();
+            Assert.AreEqual(5, c, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_C2()                 // для FindC()
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(1);
+            tr.SetB(6);
+            tr.SetAlfa(110);
+
+            tr.FindC();
+            double c = tr.GetC();
+            Assert.AreEqual(6.41, c, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Bet1()                 // для FindBeta()
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(4);
+            tr.SetAlfa(90);
+
+            tr.FindBeta();
+            double bet = tr.GetBeta();
+            Assert.AreEqual(36.87, bet, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Bet2()                 // для FindBeta()
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(1);
+            tr.SetB(6);
+            tr.SetAlfa(110);
+
+            tr.FindBeta();
+            double bet = tr.GetBeta();
+            Assert.AreEqual(8.43, bet, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Gam1()                 // для FindLast()
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(4);
+            tr.SetAlfa(90);
+
+            tr.FindLast();
+            double gam = tr.GetGamma();
+            Assert.AreEqual(53.13, gam, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Gam2()                 // для FindLast()
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(1);
+            tr.SetB(6);
+            tr.SetAlfa(110);
+
+            tr.FindLast();
+            double gam = tr.GetGamma();
+            Assert.AreEqual(61.57, gam, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_C1_not1()                 // для FindC() с неверным результатом
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(4);
+            tr.SetAlfa(90);
+
+            tr.FindC();
+            double c = tr.GetC();
+            Assert.AreEqual(56, c, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Bet1_not1()                 // для FindBeta() с неверным результатом
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(4);
+            tr.SetAlfa(90);
+
+            tr.FindBeta();
+            double bet = tr.GetBeta();
+            Assert.AreEqual(14, bet, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Gam1_not1()                 // для FindLast() с неверным результатом
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(4);
+            tr.SetAlfa(90);
+
+            tr.FindLast();
+            double gam = tr.GetGamma();
+            Assert.AreEqual(41, gam, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_C1_not2()                 // для FindC() с некорректными данными
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(-3);
+            tr.SetB(4);
+            tr.SetAlfa(90);
+
+            tr.FindC();
+            double c = tr.GetC();
+            Assert.AreEqual(5, c, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Bet1_not2()                 // для FindBeta() с некорректными данными
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(4);
+            tr.SetAlfa(200);
+
+            tr.FindBeta();
+            double bet = tr.GetBeta();
+            Assert.AreEqual(36.87, bet, 0.01);
+        }
+
+        [TestMethod]
+        public void TestMethod_Gam1_not2()                 // для FindLast() с некорректными данными
+        {
+            Triangle.Triangle tr = new Triangle.Triangle();
+            tr.SetA(3);
+            tr.SetB(-4);
+            tr.SetAlfa(181);
+
+            tr.FindLast();
+            double gam = tr.GetGamma();
+            Assert.AreEqual(53.13, gam, 0.01);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.IO.FileNotFoundException))]           
+        public void TestMethod_File()                                 
+        {
+            string[] argss = new string[] { "123.txt", "321.txt" };
+            Program.Main(argss);
+        }
+    }
+}
